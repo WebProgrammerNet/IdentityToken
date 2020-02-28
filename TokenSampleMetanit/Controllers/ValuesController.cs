321b68350e7cfefeb5ccb4a8c19e272658b0c9ea
WebProgrammerNet <webprogrammer.readonly@gmail.com>
2020-02-28 10:13:48 +0400

    first commit

---
+++ TokenSampleMetanit/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokenSampleMetanit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [Authorize]
+        [Route("getlogin")]
+        public IActionResult GetLogin()
+        {
+            return Ok($"Ваш логин: {User.Identity.Name}");
+        }
+
+        [Authorize(Roles = "admin")]//Authorizenin icinde default role var.Ro yazan kimi automatic Roles gelir.
+        [Route("getrole")]
+        public IActionResult GetRole()
+        {
+            return Ok("Ваша роль: администратор");
+        }
+    }
+}
